b3ca3e132b0ad34c134a3640c33d3e1971d9a153
otajon <otajonazamov20@gmail.com>
2021-07-08 19:25:18 +0500

    ichlari yozilyapti....

--- src/main/java/org/oag/apprestjwt/controller/HomeController.java
+++ src/main/java/org/oag/apprestjwt/controller/HomeController.java
@@ -1,6 +1,15 @@
 package org.oag.apprestjwt.controller;
 
 
-public class HomeController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/home")
+public class HomeController {
+    @GetMapping
+    public String getHomePage(){
+        return "Welecome Home Page";
+    }
 }
