49a7d8b3325453dbbede26499e56f3a83e63761e
otajon <otajonazamov20@gmail.com>
2021-07-08 19:19:12 +0500

    birinchi commid

---
+++ src/test/java/org/oag/apprestjwt/AppRestJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oag.apprestjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRestJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
